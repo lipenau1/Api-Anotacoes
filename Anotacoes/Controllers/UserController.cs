eea67351fb875c06fb98b106d8bf9dad755bae45
Felipe da Silva Nau <felipe.n@lyncas.net>
2022-02-14 21:37:53 -0300

    fixing some shit

--- Anotacoes/Controllers/UserController.cs
+++ Anotacoes/Controllers/UserController.cs
@@ -21,5 +21,82 @@ namespace Anotacoes.Controllers
             }
             return JsonConvert.SerializeObject(usuarios);
         }
+
+        [HttpGet("{id}")]
+
+        public Usuario Get(int id)
+        {
+            using var db = new AN.Api.Data.ApplicationContext();
+            return db.Usuarios.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Usuario Usuario)
+        {
+            if (!UsuarioExiste(Usuario.Id))
+            {
+                using var db = new AN.Api.Data.ApplicationContext();
+                db.Usuarios.Add(Usuario);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Usuario value)
+        {
+            using var db = new AN.Api.Data.ApplicationContext();
+            try
+            {
+                if (UsuarioExiste(id))
+                {
+                    var objUsuario = db.Usuarios.FirstOrDefault(x => x.Id == id);
+                    var obj = new
+                    {
+                        Nome = value.Nome,
+                        Email = value.Email,
+                    };
+
+                    db.Entry(objUsuario).CurrentValues.SetValues(obj);
+                    db.SaveChanges();
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (UsuarioExiste(id))
+            {
+                using var db = new AN.Api.Data.ApplicationContext();
+                db.Usuarios.Remove(db.Usuarios.FirstOrDefault(x => x.Id == id));
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        private bool UsuarioExiste(int id)
+        {
+            using var db = new AN.Api.Data.ApplicationContext();
+            return db.Usuarios.Any(e => e.Id == id);
+        }
     }
 }
+
